c500c8189ba6e08710de84c41ecf94aa387db2f1
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-05-19 22:29:20 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,33 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public double Max{get;set;}
+        public double Min{get;set;}
+        public double Average{get;set;}
+        private int total;
+        
+        public Tuple<double,double,double> CalculateStatistics(List<double?> numbers) 
+        {
+            if(numbers.Count==0)
+            {
+                Min=double.NaN;
+                Max=double.NaN;
+                Average=double.NaN;
+            }
+            else
+            {
+                numbers.Sort();
+                Min=Convert.ToDouble(numbers[0]);
+                Max=Convert.ToDouble(numbers[numbers.Count-1]);
+                
+                foreach(var count in numbers)
+                {
+                    sum+=count.Value;
+                    ++total;
+                }
+                Average=sum/total;
+            }
+            return Tuple.Create(Min,Max,Average);
         }
     }
 }
