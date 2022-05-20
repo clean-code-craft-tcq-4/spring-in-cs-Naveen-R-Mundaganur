a74ccd7bed45e15cb08ba677ab37ced82600d983
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-05-20 15:49:28 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -35,4 +35,68 @@ namespace Statistics
             return Tuple.Create(Min,Max,Average);
         }
     }
+    public interface IAlerter
+    {
+        bool EmailSent{get;set;}
+        bool LEDGlows{get;set;}
+    }
+    public class EmailAlert:IAlerter
+    {
+        bool emailSent;
+        public bool EmailSent
+        {
+            get =>emailSent;
+            set => emailSent=value;
+        }
+        public bool LEDGlows
+        {
+            get;
+            set;
+        }        
+    }
+    public class LEDAlert:IAlerter
+    {
+        bool ledGlows;
+        public bool EmailSent
+        {
+            get;
+            set;
+        }
+        public bool LEDGlows
+        {
+             get =>ledGlows;
+            set => ledGlows=value;   
+        }
+    }
+    public class StatsAlerter:IAlerter
+    {
+        readonly float max_threshold;
+        float max_output;
+        EmailAlert ealt;
+        LEDAlert ledalt;
+        
+        public StatsAlerter(float max_data, IAlerter[] ialt)
+        {
+            max_threshold=max_data;
+            ealt=ialt[0];
+            ledalt=ialt[1];
+        }
+        public void checkAndAlert(List<float> data)
+        {
+            data.Sort();
+            data.Reverse();
+            max_output=data[0];
+            if(max_output>max_threshold)
+            {
+                ealt.EmailSent=true;
+                ledalt.LEDGlows=true;
+            }
+            else
+            {
+                ealt.EmailSent=false;
+                ledalt.LEDGlows=false;
+            }                        
+        }
+        
+    }
 }
