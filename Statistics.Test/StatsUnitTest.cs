386cc78f04012a91d1a2d59b85cb9173781d7e58
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-05-21 18:48:08 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -10,10 +10,11 @@ namespace Statistics.Test
         [Fact]
         public void ReportsAverageMinMax()
         {
-            var statsComputer = new StatsComputer();
+            float epsilon = 0.001F;
+            StatsComputer statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<double>{1.5, 8.9, 3.2, 4.5});
-            float epsilon = 0.001F;
+            
             Assert.True(Math.Abs(statsComputer.Average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.Max - 8.9) <= epsilon);
             Assert.True(Math.Abs(statsComputer.Min - 1.5) <= epsilon);
@@ -21,7 +22,7 @@ namespace Statistics.Test
         [Fact]
         public void ReportsNaNForEmptyInput()
         {
-            var statsComputer = new StatsComputer();
+            StatsComputer statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<double>{});
             Assert.True(Double.IsNaN(statsComputer.Max));
@@ -34,12 +35,12 @@ namespace Statistics.Test
         [Fact]
         public void RaisesAlertsIfMaxIsMoreThanThreshold()
         {
-            var emailAlert = new EmailAlert();
-            var ledAlert = new LEDAlert();
-            IAlerter[] alerters = {emailAlert, ledAlert};
-
             const float maxThreshold = 10.2f;
-            var statsAlerter = new StatsAlerter(maxThreshold, alerters);
+            EmailAlert emailAlert = new EmailAlert();
+            LEDAlert ledAlert = new LEDAlert();
+            IAlerter[] alerters = {emailAlert, ledAlert};
+            
+            StatsAlerter statsAlerter = new StatsAlerter(maxThreshold, alerters);
             statsAlerter.checkAndAlert(new List<float>{0.2f, 11.9f, 4.3f, 8.5f});
 
             Assert.True(emailAlert.EmailSent);
